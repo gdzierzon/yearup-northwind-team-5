79af6b9d24a2d5b3a86189ebc9759bbe101e5e52
Sebastian Acosta <mu9seb@gmail.com>
2023-12-08 15:55:09 -0700

    Functionality underway.

--- northwind-traders-website/scripts/app.js
+++ northwind-traders-website/scripts/app.js
@@ -1,8 +1,33 @@
 let productService;
-let productFilters;
+
+let productFiltersMenu;
+let categoryFilterMenu;
+
 document.addEventListener('DOMContentLoaded', ()=>{
+    
     productService = new ProductsService();
-    productFilters = document.getElementById('productFilters');
-    productFilters.addEventListener('change', filterProducts);
+
+    productFiltersMenu = document.getElementById('productFiltersMenu');
+    productFiltersMenu.addEventListener('change', filterProducts);
+
+    categoryFilterMenu = document.getElementById('categoryFilterMenu');
+
+    loadData();
+
+
     
 })
+
+async function loadData() {
+    
+    const categories = await productService.getAllCategories();
+
+    categories.forEach(category => {
+        const option = new Option(category.name, category.id);
+        categoryFilterMenu.appendChild(option);
+    })
+}
+
+async function filterProducts() {
+
+}
--- northwind-traders-website/scripts/services/product-service.js
+++ northwind-traders-website/scripts/services/product-service.js
@@ -1,10 +1,12 @@
-class ProductsService{
+class ProductsService {
     
-APIurl = "http://localhost:3000/products";
-async getAll(){
-    let products = await fetch(APIurl);
-    let response = await products.json();
-    let productsArray = response.products;
-    console.log(productsArray)
-}
+    productsUrl = "http://localhost:3000/products";
+    categoriesUrl = "http://localhost:3000/categories";
+
+    async getAllCategories() {
+        let response = await fetch(this.categoriesUrl);
+        let data = await response.json();
+        console.log(data)
+        return categoriesArray;
+    }
 }
